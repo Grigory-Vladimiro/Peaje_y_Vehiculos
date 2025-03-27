4a7a7d41a0848b0064bdd60d309ab4af158cbc17
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-27 19:37:45 +0100

    method_toll_calc_add_moto

--- peaje_y_vehiculos/src/main/java/dev/grigory/peaje_y_vehiculos/TollCalculator.java
+++ peaje_y_vehiculos/src/main/java/dev/grigory/peaje_y_vehiculos/TollCalculator.java
@@ -5,4 +5,7 @@ public class TollCalculator {
     public static int calculate(Car car) {
         return 100;
     }
+    public static int calculate(Motorcycle moto) {
+        return 50;
+    }
 }
