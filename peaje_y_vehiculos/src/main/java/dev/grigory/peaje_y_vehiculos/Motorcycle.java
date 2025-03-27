7b52d8f0b92c5aff35a1ae7977726fe7b99b6d34
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-27 19:34:51 +0100

    moto_class

---
+++ peaje_y_vehiculos/src/main/java/dev/grigory/peaje_y_vehiculos/Motorcycle.java
@@ -0,0 +1,13 @@
+package dev.grigory.peaje_y_vehiculos;
+
+public class Motorcycle {
+    private String plate;
+
+    public Motorcycle(String plate) {
+        this.plate = plate;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+}
