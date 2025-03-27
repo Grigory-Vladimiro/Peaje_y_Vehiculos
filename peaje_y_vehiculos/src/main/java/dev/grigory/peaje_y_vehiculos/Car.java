c85c850f74e27e04f377b59aa63f72ff7c88194b
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-27 12:09:16 +0100

    method_car_plate

---
+++ peaje_y_vehiculos/src/main/java/dev/grigory/peaje_y_vehiculos/Car.java
@@ -0,0 +1,14 @@
+package dev.grigory.peaje_y_vehiculos;
+
+public class Car {
+
+    private String plate;
+
+    public Car(String plate) {
+        this.plate = plate;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+}
